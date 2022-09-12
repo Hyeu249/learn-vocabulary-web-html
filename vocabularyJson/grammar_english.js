7ded55f32a6b8069afa3d254abdca102a268ebc2
BaoHieu <labaohieu@gmail.com>
2022-09-12 07:55:08 +0700

    initial

---
+++ vocabularyJson/grammar_english.js
@@ -0,0 +1,32 @@
+import ANSWER_KEY from "./test1.js";
+
+const json = [
+  // "____________________CLAUSE OR N/VING____________________",
+  // "after: sau khi",
+  // "____________________WORD + CLAUSE____________________",
+  // "whom: ai, người mà(Ny + whom + S + V)",
+  // "even though: mặc dù",
+  // "while: trong khi(dùng nói về sự việc # và ss)",
+  // "just as: giống như",
+  // "____________________WORD + N/VING____________________",
+  // "of: của, về, vì,..",
+  // "thereby: do đó, qua đó",
+  // "notwithstanding: bất chấp",
+  // "regardless of: bất chấp",
+  // "____________________N(nhiều) OR N(KĐĐ)____________________",
+  // "a lot of: nhiều"
+  // "____________________WORD + N(nhiều)____________________",
+  // "many: nhiều",
+  // "few: ít"
+  // "____________________WORD + N(KĐĐ)____________________",
+  // "much: nhiều",
+  // "____________________BỔ NGỮ CHO ĐỘNG TỪ(VERB + WORD)____________________",
+  // "a lot: nhiều",
+];
+
+export { json as namE };
+// export { ANSWER_KEY as namE };
+
+//Create vocabulary convention
+//nếu 1 từ, đặt bình thường
+//nếu 2 từ, thêm dấu chấm cuối câu(nếu có dấu đóng ngoặc, cũng thêm dấu chấm sau nó)
